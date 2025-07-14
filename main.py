1a8151dfc9e5b9d49b437d921d52bb8e40007cab
priyanshu-gairola <gairola.priyanshu03@gmail.com>
2025-07-14 14:55:58 +0530

    Added schema validaytion

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app=FastAPI()
+
+class Post(BaseModel):
+  title:str
+  content:str
+  published:bool=True
+  rating:Optional[int]=None
 
 @app.get('/')
 def root():
@@ -9,4 +18,13 @@ def root():
 @app.get('/hello/{name}')
 def hello(name):
   return f"Welcome {name}"
+
+@app.post("/createpost")
+def create_post(post:Post):
+  print(post.title)
+  print(post.content)
+  print(post.rating)
+  return post
+
+
 
