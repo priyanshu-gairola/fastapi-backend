e9600ef3eacb7483eb9a358b755dd70663001f21
priyanshu-gairola <gairola.priyanshu03@gmail.com>
2025-07-15 12:38:57 +0530

    Adeed posts and introduction to returning status codes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app=FastAPI()
 
@@ -11,20 +12,51 @@ class Post(BaseModel):
   published:bool=True
   rating:Optional[int]=None
 
+my_posts=[
+   {"title":"Kandara",
+    "content":"Beautiful village in uttarakhand",
+    "published":True,
+    "rating":10,
+    "id":1
+    },
+    {
+    "title":"Ddun",
+    "content":"Beautiful city in uttarakhand",
+    "published":False,
+    "rating":8,
+    "id":2
+    }
+    ]
+
+def find_post(id):
+  for post in my_posts:
+    if post["id"]==id:
+      return post
+    
+
 @app.get('/')
 def root():
   return {"message":"Hey ! made by Priyanshu"}
 
-@app.get('/hello/{name}')
-def hello(name):
-  return f"Welcome {name}"
+@app.get('/posts')
+def get_all_posts():
+  return my_posts
 
-@app.post("/createpost")
+@app.get("/posts/{id}")
+def get_post_by_id(id:int):
+  post=find_post(id)
+  if not post:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"post with id:{id} not found."
+    )
+  return {"post_details":post}
+
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_post(post:Post):
-  print(post.title)
-  print(post.content)
-  print(post.rating)
-  return post
+  post_dict=post.dict()
+  post_dict["id"]=randrange(0,100000)
+  my_posts.append(post_dict)
+  return {"data":post_dict}
 
 
 
