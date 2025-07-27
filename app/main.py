8df90d2b912aa73fbaa60779b1dd019857a7cb74
priyanshu-gairola <gairola.priyanshu03@gmail.com>
2025-07-27 21:37:10 +0530

    Added routes to get user details by id

--- app/main.py
+++ app/main.py
@@ -152,4 +152,15 @@ def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):
 
   return new_user
 
+#get users by id
+
+@app.get("/users/{id}",response_model=schemas.UserOut)
+def get_user_by_ID(id:int,db:Session=Depends(get_db)):
+  user=db.query(models.User).filter(models.User.id==id).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id:{id} not found")
+  return user
+
+
 
